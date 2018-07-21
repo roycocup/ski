3f464212c07e95b2b2568086f05616d750cb3a3c
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-07-22 00:25:06 +0100

    more

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
 export (int) var speed = 100
-var region = {x=0, y=0, w=0, h=0}
-enum statuses {RESET, LEFT1, LEFT2, RIGHT1, RIGHT2}
-var status = statuses.RESET
-signal reset_background
 
-var FSM = preload("FSM.gd")
+var region = {x=0, y=0, w=0, h=0}
+
+enum States {RESET, LEFT1, LEFT2, RIGHT1, RIGHT2}
+var status = States.RESET
+
+signal reset_background
 
 func _ready():
 	animate_reset()
@@ -20,27 +21,27 @@ func _physics_process(delta):
 	
 func input_listen(delta):
 	if Input.is_action_just_pressed("ui_right"):
-		if status == statuses.RIGHT1:
-			status = statuses.RIGHT2
+		if status == States.RIGHT1:
+			status = States.RIGHT2
 			region = {x=276, y=11, w=83, h=74} 
 			linear_damp = .5
 		else:
-			status = statuses.RIGHT1
+			status = States.RIGHT1
 			region = {x=158, y=10, w=60, h=100}
 			linear_damp = .1
 		linear_velocity = Vector2(speed * .75, speed * .75)
 		set_animate_region()
 	if Input.is_key_pressed(KEY_LEFT):
-		if status == statuses.LEFT1:
-			status = statuses.LEFT2
+		if status == States.LEFT1:
+			status = States.LEFT2
 			region = {x=276, y=11, w=83, h=74}
 		else: 
-			status = statuses.LEFT1 
+			status = States.LEFT1 
 			region = {x=158, y=10, w=60, h=100} 
 		linear_velocity = Vector2(-speed * .75, speed * .75)
 		set_animate_region(true)
 	if Input.is_key_pressed(KEY_DOWN):
-		status = statuses.RESET 
+		status = States.RESET 
 		linear_velocity = Vector2(0, speed)
 		animate_reset()
 
@@ -66,3 +67,5 @@ func check_reset_background():
 	var x = fmod(position.y, 512)
 	if (x > 0 and x < 1):
 		emit_signal("reset_background")
+
+	
---
+++ Scripts/TODO.md
@@ -0,0 +1,7 @@
+# TODO LIST
+
+1. FSM for player
+2. Backgrounds changing
+3. Obstacles
+4. Jumps
+5. UI timer
