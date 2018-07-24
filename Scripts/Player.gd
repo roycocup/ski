c9be62f67c1c0d86cf91a4de53a26e44d9806e9f
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-07-24 22:43:11 +0100

    starting FSM

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,53 +1,90 @@
 extends RigidBody2D
 
 export (int) var speed = 100
-
 var region = {x=0, y=0, w=0, h=0}
 var howFarDown = 0
 var lastMark = 0
-
-enum States {RESET, LEFT1, LEFT2, RIGHT1, RIGHT2}
-var status = States.RESET
+onready var FSM = preload("res://Scripts/FSM.gd").new()
 
 signal create_new_background
 
+
 func _ready():
+	init_states()
 	animate_reset()
 	linear_velocity.y = speed
 
-
 func _physics_process(delta):
 	input_listen(delta)
+	animate()
 	check_reset_background()
 	
-	
+func init_states():
+	FSM.add_state(
+		FSM.states.RESET, [
+			{"event":FSM.events.LEFT, "state":FSM.states.LEFT1}, 
+			{"event":FSM.events.RIGHT, "state":FSM.states.RIGHT1}, 
+		])
+	# FSM.add_state(FSM.states.LEFT1, [FSM.states.LEFT2, FSM.states.RESET])
+	# FSM.add_state(FSM.states.LEFT2, [FSM.states.LEFT1])
+	# FSM.add_state(FSM.states.RIGHT1, [FSM.states.RIGHT2, FSM.states.RESET])
+	# FSM.add_state(FSM.states.RIGHT2, [FSM.states.RIGHT1])
+	FSM.current_state = FSM.states.RESET
+
 func input_listen(delta):
 	if Input.is_action_just_pressed("ui_right"):
-		if status == States.RIGHT1:
-			status = States.RIGHT2
-			region = {x=276, y=11, w=83, h=74} 
-			linear_damp = .5
-		else:
-			status = States.RIGHT1
-			region = {x=158, y=10, w=60, h=100}
-			linear_damp = .1
-		linear_velocity = Vector2(speed * .75, speed * .75)
-		set_animate_region()
+		FSM.handle(FSM.events.RIGHT)
 	if Input.is_action_just_pressed("ui_left"):
-		if status == States.LEFT1:
-			status = States.LEFT2
-			region = {x=276, y=11, w=83, h=74}
-			linear_damp = .5
-		else: 
-			status = States.LEFT1 
-			region = {x=158, y=10, w=60, h=100} 
-			linear_damp = .1
-		linear_velocity = Vector2(-speed * .75, speed * .75)
-		set_animate_region(true)
-	if Input.is_key_pressed(KEY_DOWN):
-		status = States.RESET 
+		FSM.handle(FSM.events.LEFT)
+
+func animate():
+	if FSM.current_state == FSM.states.RESET:
 		linear_velocity = Vector2(0, speed)
 		animate_reset()
+		return
+	if FSM.current_state == FSM.states.RIGHT1:
+		region = {x=158, y=10, w=60, h=100}
+		linear_damp = .1
+	if FSM.current_state == FSM.states.RIGHT2:
+		region = {x=276, y=11, w=83, h=74} 
+		linear_damp = .5
+	if FSM.current_state == FSM.states.LEFT1:
+		region = {x=158, y=10, w=60, h=100} 
+		linear_damp = .1
+	if FSM.current_state == FSM.states.LEFT2:
+		region = {x=276, y=11, w=83, h=74}
+		linear_damp = .5
+	
+	linear_velocity = Vector2(-speed * .75, speed * .75)
+	set_animate_region(true)
+
+# func input_listen_old(delta):
+# 	if Input.is_action_just_pressed("ui_right"):
+# 		if status == States.RIGHT1:
+# 			status = States.RIGHT2
+# 			region = {x=276, y=11, w=83, h=74} 
+# 			linear_damp = .5
+# 		else:
+# 			status = States.RIGHT1
+# 			region = {x=158, y=10, w=60, h=100}
+# 			linear_damp = .1
+# 		linear_velocity = Vector2(speed * .75, speed * .75)
+# 		set_animate_region()
+# 	if Input.is_action_just_pressed("ui_left"):
+# 		if status == States.LEFT1:
+# 			status = States.LEFT2
+# 			region = {x=276, y=11, w=83, h=74}
+# 			linear_damp = .5
+# 		else: 
+# 			status = States.LEFT1 
+# 			region = {x=158, y=10, w=60, h=100} 
+# 			linear_damp = .1
+# 		linear_velocity = Vector2(-speed * .75, speed * .75)
+# 		set_animate_region(true)
+# 	if Input.is_key_pressed(KEY_DOWN):
+# 		status = States.RESET 
+# 		linear_velocity = Vector2(0, speed)
+# 		animate_reset()
 
 func animate_reset():
 	region = {x=35, y=10, w=49, h=97} 
